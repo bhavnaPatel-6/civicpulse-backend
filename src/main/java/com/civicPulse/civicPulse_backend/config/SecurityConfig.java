b923a44608384d5d764b026573ac78ac30d73ef6
bhavnaPatel-6 <pbhavna0987@gmail.com>
2026-09-21 08:42:25 +0530

    Configure Swagger

--- src/main/java/com/civicPulse/civicPulse_backend/config/SecurityConfig.java
+++ src/main/java/com/civicPulse/civicPulse_backend/config/SecurityConfig.java
@@ -33,6 +33,10 @@ public class SecurityConfig {
                                 "/api/auth/register",
                                 "/api/auth/login",
                                 "/api/test"
+                                ,
+                                "/swagger-ui/**",
+                                "/swagger-ui.html",
+                                "/v3/api-docs/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
